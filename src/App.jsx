ea17755ea5854e44c2c1d7fea2ad08f02dac6ecd
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-01-10 09:19:43 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Header } from './components/header/Header'
 import { Main } from './components/main/Main'
 import { Footer } from './components/footer/Footer'
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Header className="reset"> 
+        <h1>Velkommen</h1>
+      </Header>
+      <Main> 
+        <h2>Dette er main</h2>
+      </Main>
+      <Footer>
+        <h3>Footer</h3>
+        <a href="#">Test</a>
+      </Footer>
     </>
   )
 }
